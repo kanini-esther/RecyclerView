09745f0e2f02731c43bf6fc5838a53302d1457c1
Kanini-esther <esther.njeri.kanini@gmail.com>
2020-08-06 00:00:29 +0300

    my first commit

--- app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -4,18 +4,21 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import layout.NamesRecyclerViewAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        RecycleView_Main.setBackgroundColor(Color.CYAN)
+//        RecyclerView_Main.setBackgroundColor(Color.CYAN)
         rvNames.LayoutManager = LinearLayoutManager(baseContext)
 
-        val namesAdapter = NamesRecyclerViewAdapter(ListOf("Esther","Mercy","caro","Berril","Faith","Marriam","Natasha","Nikita","shirley",))
+        val namesAdapter = NamesRecyclerViewAdapter(listOf("Berril","Faith","Duncan","Caro","Marian","Nancy","Eunice","Monica","Martha","Moses",))
 
+        rvNames.adapter = namesAdapter
 
     }
 }
